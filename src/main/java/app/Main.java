d1e519dbaaf673b200ccb79ac8e57d94a26a4a04
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2024-04-03 12:29:53 +0200

    woops

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,8 +3,15 @@ package app;
 import app.config.ThymeleafConfig;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
+import org.postgresql.jdbc2.optional.ConnectionPool;
 
 public class Main {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
     public static void main(String[] args)
     {
         // Initializing Javalin and Jetty webserver
