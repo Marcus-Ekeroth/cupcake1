4f16d6aba4974b759ed5e02d8ee20689e0f38a12
DeZoete <143606680+DeZoete@users.noreply.github.com>
2024-04-04 11:04:58 +0200

    UserMapper fixed exceptions

--- src/main/java/app/exceptions/DatabaseException.java
+++ src/main/java/app/exceptions/DatabaseException.java
@@ -1,9 +1,17 @@
 package app.exceptions;
 
-import app.logging.AppLog;
-
 public class DatabaseException extends Exception
 {
-    public DatabaseException(String userMessage) {
+    public DatabaseException(String userMessage)
+    {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+    }
+
+    public DatabaseException(String userMessage, String systemMessage)
+    {
+        super(userMessage);
+        System.out.println("userMessage: " + userMessage);
+        System.out.println("errorMessage: " + systemMessage);
     }
 }
