78480c21821caefe6eb960a8973c76b598e6214a
Freddo-404 <cph-fd101@cphbusiness.dk>
2024-04-05 12:59:45 +0200

    admin page

---
+++ src/main/java/app/controllers/BottomController.java
@@ -0,0 +1,16 @@
+package app.controllers;/* @auther: Frederik Dupont */
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class BottomController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+    }
+
+    public static void getAllBottom(Context ctx, ConnectionPool connectionPool){
+
+    }
+}
+
+
